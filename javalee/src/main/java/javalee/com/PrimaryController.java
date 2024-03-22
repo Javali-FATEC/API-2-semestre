f386e2d247d709d7908cd4db438a53ba5486dd01
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-03-22 19:05:07 -0300

    :sparkles: :feat: Criação da tela de importação do CSV

---
+++ javalee/src/main/java/javalee/com/PrimaryController.java
@@ -0,0 +1,12 @@
+package javalee.com;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}

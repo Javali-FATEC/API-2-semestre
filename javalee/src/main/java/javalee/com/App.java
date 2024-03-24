7e34a9996cac6985557762e20c698f2b2812b5a1
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-03-24 11:00:22 -0300

    :ok_hand: fix: interface tweaks

--- javalee/src/main/java/javalee/com/App.java
+++ javalee/src/main/java/javalee/com/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,10 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("importCSV"));
         stage.setScene(scene);
+        // stage.getIcons().add(new Image("/javalee.jpg"));
+        // C:\Users\vinic\OneDrive\Área de Trabalho\fatec\2º semestre\API-2-semestre\javalee\src\images\javalee.jpg
+        // C:\Users\vinic\OneDrive\Área de Trabalho\fatec\2º semestre\API-2-semestre\javalee\src\main\java\javalee\com\App.java
+        stage.setTitle("Importar CSV");
         stage.show();
     }
 

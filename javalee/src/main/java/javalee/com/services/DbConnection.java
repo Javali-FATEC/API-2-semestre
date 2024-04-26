553ee2017679b7ccd0b202fe009cd7d8a34f68ee
Isaque de Souza <Isaquesc2@gmail.com>
2024-04-26 16:55:56 -0300

    #32 - Adicionado a conexão com o Banco de dados

---
+++ javalee/src/main/java/javalee/com/services/DbConnection.java
@@ -0,0 +1,40 @@
+package javalee.com.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    public Connection connect_to_db(String dbname, String user, String password) {
+
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, password);
+            if (conn != null) {
+                System.out.println("Conexão realizada com sucesso");
+            } else {
+                System.out.println("Falha na conexão");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    public void Desconnect(Connection conn) {
+
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- javalee/src/main/java/module-info.java
+++ javalee/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module javalee.com {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens javalee.com to javafx.fxml;
     exports javalee.com;

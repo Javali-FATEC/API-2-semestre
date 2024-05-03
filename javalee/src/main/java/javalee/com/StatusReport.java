483fee370364fbc905528f6f6244dc9cce913507
santosc137 <cs@moray.ai>
2024-05-03 19:48:27 -0300

    Relatório situacional

---
+++ javalee/src/main/java/javalee/com/StatusReport.java
@@ -0,0 +1,70 @@
+package javalee.com;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javalee.com.bd_connection.DbConnection;
+import javafx.event.ActionEvent;
+
+public class StatusReport {
+
+    private ObservableList<String> cityList = FXCollections.observableArrayList();
+
+    @FXML
+    private ChoiceBox<String> cityChoiceBox;
+
+    private String citySelected;
+
+    
+    @FXML
+    private void initialize() throws SQLException {
+        DbConnection db = new DbConnection();
+        ResultSet cityResult = db.executeWithReturn("SELECT * FROM cidade");
+        db.Desconnect();
+        if (cityResult == null) {
+            return;
+        }
+        while (cityResult.next()) {
+            String cityName = cityResult.getString("nome_cidade");
+            cityList.add(cityName);
+
+        }
+        cityChoiceBox.setItems(cityList);
+        System.out.println(cityList);
+    }
+
+
+    @FXML
+    private void generateReport(ActionEvent event) {
+        DbConnection db = new DbConnection();
+
+        String citySelected = cityChoiceBox.getValue();
+        List<String> estacoes = getEstacoes();
+        if (estacoes == null) {
+            return;
+        }
+    };
+
+    private List getEstacoes() throws SQLException {
+        DbConnection db = new DbConnection();
+        String query_estacao = "SELECT * FROM estacao WHERE nome_cidade = '" + citySelected + "'";
+        List<String> ids_estations = new ArrayList<String>();
+        ResultSet resultEstacoes = db.executeWithReturn(query_estacao);
+        db.Desconnect();
+    
+        if (resultEstacoes == null) {
+            return ids_estations;
+        }
+        while (resultEstacoes.next()) {
+            String estation_id = resultEstacoes.getString("id_estacao");
+            ids_estations.add(estation_id);
+
+        }
+        return ids_estations;
+    };
+}

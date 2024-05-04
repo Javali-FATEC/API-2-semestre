0b6c975c225233a5803ca39bc54f723d1f71c261
santosc137 <cs@moray.ai>
2024-05-04 13:26:57 -0300

    Registros encontrados

--- javalee/src/main/java/javalee/com/StatusReportController.java
+++ javalee/src/main/java/javalee/com/StatusReportController.java
@@ -1,4 +1,5 @@
 package javalee.com;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -7,12 +8,17 @@ import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 import javalee.com.bd_connection.DbConnection;
 import javafx.event.ActionEvent;
 import java.util.HashMap;
 
 public class StatusReportController {
+
+    private static Scene scene;
 
     @FXML
     private ObservableList<String> cityList = FXCollections.observableArrayList();
@@ -20,12 +26,13 @@ public class StatusReportController {
     @FXML
     private ComboBox<String> cityChoiceBox;
 
-    private int id_city;
+    private String citySelected;
 
-    private List<String> estacoes_ids;
+    private List<String> estacoes_ids = new ArrayList<>();;
+
+    private HashMap<String, HashMap<String, String>> mediasResults = new HashMap<String, HashMap<String, String>>();
 
     private HashMap<String, String> metricasCadastradasDB = new HashMap<String, String>();
-
     
     @FXML
     private void initialize() throws SQLException {
@@ -42,58 +49,60 @@ public class StatusReportController {
         ResultSet metricasCadastradas = helpDB("SELECT * FROM metrica");
         if (metricasCadastradas != null) {
             while (metricasCadastradas.next()) {
-                metricasCadastradasDB.put(metricasCadastradas.getString("id_metrica"), metricasCadastradas.getString("nome_metrica"));
+                metricasCadastradasDB.put(metricasCadastradas.getString("id_metrica"), metricasCadastradas.getString("nome"));
             }
         }
     }
 
-
     @FXML
     private void reportGenerate(ActionEvent event) throws SQLException {
-        System.out.println("Gerar relatório");
-        String citySelected = cityChoiceBox.getValue();
-        System.out.println(citySelected);
+        citySelected = cityChoiceBox.getValue();
         if (citySelected == null) {
             return;
         }
-        String sql = "SELECT * FROM cidade WHERE nome_cidade = '" + citySelected + "'";
-        ResultSet cityInformation = helpDB(sql);
-        if(cityInformation.next()){
-            id_city = cityInformation.getInt("id_cidade");
+        reportGenerateMediaDate();
+        if (mediasResults.isEmpty()) {
+            return;
         }
-        estacoes_ids = getEstacoesIds();
-        if (estacoes_ids == null) {
-            estacoes_ids = new ArrayList<>();
-        }
-        ResultSet mediaResult = getMediaResultsFromIdList();
 
-        while (mediaResult.next()) {
-            System.out.println(mediaResult.getString("valor"));
-            
-        }
+
+        System.out.println("Gerar relatório");
+                
     };
 
-    private List<String> getEstacoesIds() throws SQLException {
-        String sql = "SELECT * FROM estacao WHERE id_cidade = '" + id_city + "'";
+    private void reportGenerateMediaDate() throws SQLException{
+        getEstacoesIds();
+        getMediaResultsFromIdList();
+    }
+
+    private void getEstacoesIds() throws SQLException {
+        String sql = "SELECT e.id_estacao FROM cidade c JOIN estacao e ON c.id_cidade = e.id_cidade WHERE c.nome_cidade = '" + citySelected + "'";
         ResultSet resultEstacoes = helpDB(sql);
-        List<String> ids_estations = new ArrayList<String>();
-    
         if (resultEstacoes == null) {
-            return ids_estations;
+            return;
         }
         while (resultEstacoes.next()) {
             String estation_id = resultEstacoes.getString("id_estacao");
-            ids_estations.add(estation_id);
-
+            estacoes_ids.add(estation_id);
         }
-        return ids_estations;
     };
 
-    private ResultSet getMediaResultsFromIdList() {
+    private void getMediaResultsFromIdList() throws SQLException{
         String ids = estacoes_ids.toString().replace("[", "(").replace("]", ")");
-        String sql = "SELECT id_metrica, AVG(valor) AS media FROM registro WHERE id_estacao IN" + ids + " GROUP BY id_metrica";
+        String sql = "SELECT m.nome, r.id_metrica, AVG(valor) AS media FROM registro r JOIN metrica m ON r.id_metrica = m.id_metrica WHERE id_estacao IN" + ids + " GROUP BY r.id_metrica, m.nome";
         ResultSet resultResults = helpDB(sql);
-        return resultResults;
+        if (resultResults == null) {return;}
+        while(resultResults.next()){
+            String metrica_id = resultResults.getString("id_metrica");
+            String metrica_nome = resultResults.getString("nome");
+            String media = resultResults.getString("media");
+        
+            HashMap<String, String> innerMap = new HashMap<>();
+            innerMap.put("metrica_nome", metrica_nome);
+            innerMap.put("media", media);
+        
+            mediasResults.put(metrica_id, innerMap);
+        }
     }
 
     private ResultSet helpDB(String query) {
@@ -102,4 +111,5 @@ public class StatusReportController {
         db.Desconnect();
         return resultSet;
     }
-}
+
+};
